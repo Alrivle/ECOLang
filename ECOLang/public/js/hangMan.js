49a12b7020105bc9058a723ccfff66bd7d8fbde7
Victor Molina <victor98@ciencias.unam.mx>
2019-11-28 18:30:41 -0600

    Nuevas palabras hangman

--- ECOLang/public/js/hangMan.js
+++ ECOLang/public/js/hangMan.js
@@ -1,6 +1,62 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
 class Game {
   constructor() {
-    this.dictionary = ["letter", "form", "explode", "frame", "greet", "animated", "disturbed", "entertaining", "meddle", "birds", "whistle", "dead", "slippery", "act", "evanescent", "cushion", "sign", "sticky", "screeching", "anxious", "glamorous", "spiky", "slow", "mark", "sisters", "amuck", "beef", "fantastic", "remind", "afraid", "deranged", "trap", "cover", "stage", "ship", "steep", "resonant", "profuse", "typical", "uneven", "apathetic", "damaging", "quiver", "calculator", "veil", "capable", "event", "coal", "part", "lazy", "visitor", "flame", "voiceless", "mate", "lame", "channel", "grubby", "spare", "marvelous", "even", "solid", "magnificent", "shame", "hug", "year", "club", "route", "aquatic", "hollow", "eye", "elegant", "taste", "straight", "door", "cattle", "annoy", "neat", "touch", "bucket", "snow", "wakeful", "standing", "clear", "step", "damage", "hum", "synonymous", "pocket", "month", "arrive", "dazzling", "physical", "walk", "run", "shirt", "level", "battle", "woozy", "scarf", "towering", "reign", "point", "raise", "communicate", "taste", "men", "treat", "sloppy", "lackadaisical", "coach", "bead", "embarrass", "reproduce", "fluffy", "flashy", "shiny", "charming", "great", "spicy", "skillful", "early", "materialistic", "purring", "wicked", "name", "ethereal", "boast", "north", "shallow", "steady", "ancient", "seat", "alluring", "fascinated", "classy", "preserve", "succeed", "acoustic", "twig", "fly", "porter", "piquant", "look", "afterthought", "plastic", "lock", "friend", "false", "stop", "story", "teeny", "anger", "tart", "wriggle", "bawdy", "hill", "scold", "soggy", "money", "tray", "detect", "obsolete", "obnoxious", "alleged", "yell", "curl", "graceful", "imagine", "frequent", "back", "cloudy", "land", "astonishing", "oval", "snow", "pumped", "surround", "outstanding", "mailbox", "soap", "brave", "punch", "eager", "easy", "precious", "scrawny", "beg", "stuff", "kitty", "yarn", "pancake", "pedal", "excuse", "breakable", "yawn", "lumber", "preach", "naive", "call", "heap", "flat", "chicken", "spotted", "macabre", "suffer", "tawdry", "ugly", "scarecrow", "skate", "remarkable", "playground", "table", "jellyfish", "sore", "unsightly", "laughable", "innate", "wanting", "invent", "various", "plucky", "cool", "hospital", "vast", "judge", "land", "temporary", "line", "quill", "care", "spark", "drain", "ultra", "knowledgeable", "foot", "book", "dynamic", "tasteless", "heartbreaking", "doubtful", "sable", "disagree", "colossal", "feeble", "last", "repair", "flesh", "strip", "grip", "heat", "scattered", "memory", "duck", "use", "charge", "decisive", "tree", "moor", "ray", "lethal", "nod", "behave", "promise", "sun", "truck", "riddle", "rich", "bitter", "string", "deserve", "momentous", "practice", "pop", "bells", "kind", "able", "crash", "half", "fix", "cagey", "dark", "dizzy", "hammer", "name", "hard", "rabid", "marked", "voracious", "team", "condition", "verdant", "cap", "handsome", "hour", "secretary", "vase", "tense", "tasty", "support", "stone", "nonchalant", "monkey", "rambunctious", "increase", "obeisant", "coherent", "rat", "gorgeous", "vivacious", "fretful", "slow", "healthy", "cent", "certain", "low", "science", "earthy", "space", "previous", "basketball", "peck", "damaged", "drawer", "elastic", "gather", "mourn", "fail", "imaginary", "magenta", "stereotyped", "recess", "guarantee", "man", "milk", "copper", "kiss", "station", "racial", "fixed", "eatable", "boy", "ticket", "heal", "misty", "fear", "provide", "trade", "beam", "secret", "roll", "zonked", "pull", "lewd", "own", "halting", "electric", "crabby", "cream", "abaft", "fresh", "search", "cherries", "feigned", "notebook", "rotten", "join", "spotless", "arm", "island", "value", "donkey", "attempt", "wet", "stitch", "sail", "friction", "groan", "quickest", "literate", "necessary", "sleepy", "bird", "miniature", "wrench", "sturdy", "limping", "expansion", "bore", "rose", "meal", "butter", "wretched", "milk", "toothsome", "upset", "minute", "frightened", "crooked", "fierce", "spark", "rare", "screw", "dinosaurs", "harsh", "omniscient", "snail", "screw", "texture", "abortive", "base", "poke", "trick", "five", "loose", "file", "harmony", "manage", "undesirable", "notice", "trick", "crowd", "gray", "snore", "wandering", "bless", "prefer", "barbarous", "pretty", "rail", "mammoth", "boiling", "unusual", "rapid", "chin", "tug", "train", "troubled", "announce", "bleach", "dream", "flowers", "show", "wistful", "willing", "observe", "interrupt", "knot", "middle", "laborer", "desire", "fruit", "dress", "exultant", "squeak", "adhesive", "bent", "moaning", "desert", "offend", "adamant", "filthy", "flash", "caption", "self", "root", "add", "berry", "worthless", "creator", "like", "shop", "reject", "treatment", "fork", "clammy", "farm", "unadvised", "swift", "tightfisted", "badge", "rice", "number", "branch", "eminent", "brush", "stove", "fair", "things", "bulb", "airplane", "rule", "impartial", "fear", "mountain", "permit", "high", "pale", "sore", "hot", "bee", "thought", "unnatural", "three", "friendly", "jittery", "wrong", "enormous", "abrasive", "apologize", "control", "testy", "grease", "hypnotic", "look", "action", "thing", "cake", "pleasure", "premium", "rabbit", "punish", "tranquil", "reflective", "zoom", "brief", "itchy", "youthful", "note", "observant", "quicksand", "try", "pollution", "dapper", "crowded", "tour", "ashamed", "scare", "welcome", "waggish", "rock", "order", "houses", "shoe", "birth", "round", "lick", "driving", "side", "stir", "hulking", "transport", "frail", "pump", "yard", "claim", "ripe", "phone", "war", "brown", "gamy", "uptight", "plastic", "bedroom", "geese", "title", "hideous", "obedient", "dress", "mass", "volleyball", "callous", "faded", "tail", "relax", "question", "lovely", "rightful", "tan", "cars", "conscious", "groovy", "slap", "fanatical", "camera", "faint", "lively", "form", "boil", "peel", "tasteful", "songs", "children", "purpose", "nippy", "tub", "tip", "oceanic", "radiate", "hallowed", "hissing", "hate", "brake", "arrest", "plausible", "potato", "weak", "box", "aback", "party", "pot", "tremendous", "inconclusive", "comfortable", "delicate", "grade", "living", "free", "ghost", "experience", "highfalutin", "smile", "trip", "describe", "suppose", "hammer", "colorful", "glove", "head", "waiting", "true", "rub", "drum", "improve", "play", "hose", "mark", "sense", "wax", "support", "drunk", "repulsive", "dare", "hungry", "shoes", "rely", "jelly", "jumbled", "lake", "ambiguous", "stupid", "wool", "nimble", "kick", "spurious", "trees", "current", "need", "deeply", "arrogant", "throat", "flag", "curtain", "unite", "eyes", "doubt", "woebegone", "tacit", "quixotic", "thoughtful", "calm", "lie", "disastrous", "sulky", "giants", "ossified", "swing", "lace", "cry", "unaccountable", "amazing", "market", "scent", "beautiful", "scintillating", "inject", "kick", "bad", "spotty", "brass", "placid", "vessel", "zephyr", "instruct", "excite", "start", "clover", "hot", "versed", "nest", "spy", "lunchroom", "tendency", "pear", "pail", "equable", "mute", "branch", "cart", "nutritious", "bike", "industry", "change", "country", "step", "sordid", "growth", "beds", "invincible", "needle", "shelf", "advertisement", "structure", "books", "messy", "glue", "close", "scrape", "mushy", "shivering", "bell", "trust", "wrathful", "utopian", "condemned", "fence", "deadpan", "identify", "rainstorm", "glow", "metal", "blush", "locket", "thinkable", "famous", "futuristic", "chilly", "mix", "snobbish", "nonstop", "foregoing", "third", "whip", "dislike", "useful", "request", "rainy", "huge", "plantation", "zipper", "baseball", "squealing", "fry", "machine", "long", "mysterious", "splendid", "picture", "sassy", "burn", "flavor", "collar", "post", "cable", "bathe", "saw", "defective", "precede", "bright", "lumpy", "lively", "wish", "tangy", "pets", "past", "tremble", "cluttered", "moon", "silver", "faulty", "unkempt", "satisfy", "drip", "rampant", "park", "wail", "shake", "ajar", "laugh", "phobic", "nondescript", "righteous", "invite", "needy", "railway", "eggnog", "unsuitable", "destruction", "pigs", "tramp", "van", "scatter", "abounding", "broken", "stitch", "calendar", "expensive", "remain", "military", "airport", "tame", "spell", "discreet", "bomb", "cup", "amuse", "town", "tough", "peace", "silk", "suit", "threatening", "ruddy", "toothbrush", "nerve", "dog", "vacuous", "six", "summer", "sort", "functional", "clap", "worried", "guard", "terrify", "sweltering", "cheat", "noxious", "card", "perform", "arrange", "society", "move", "milky", "reason", "bomb", "love", "industrious", "bow", "trouble", "twist", "attack", "tedious", "spray", "water", "difficult", "clever", "sister", "erratic", "scene", "quaint", "spoon", "finicky", "impulse", "immense", "brainy", "annoyed", "pleasant", "orange", "toad", "check", "zebra", "trade", "dreary", "cellar", "young", "sound", "push", "interest", "oatmeal", "sharp", "bubble", "idea", "include", "hellish", "sea", "approval", "ink", "poison", "cannon", "ring", "yoke", "nifty", "regret", "swim", "pipe", "paint", "view", "horses", "follow", "same", "overflow", "bury", "comparison", "mere", "influence", "scandalous", "complain", "cold", "superb", "pass", "reading", "nauseating", "pat", "hurt", "fluttering", "circle", "basin", "husky", "spectacular", "womanly", "taboo", "peaceful", "territory", "dependent", "tense", "ill", "count", "plant", "unbiased", "vest", "bustling", "worm", "stop", "keen", "tacky", "agreeable", "writer", "grandmother", "rifle", "careless", "vulgar", "school", "frighten", "dam", "craven", "ablaze", "mine", "innocent", "abhorrent", "abusive", "grain", "fireman", "miscreant", "furniture", "ambitious", "offbeat", "eggs", "gigantic", "wink", "didactic", "roasted", "cough", "skin", "abundant", "overjoyed", "addition", "slope", "abject", "person", "wash", "ground", "rate", "cheer", "mellow", "zealous", "attractive", "appear", "fabulous", "dance", "arithmetic", "numberless", "offer", "steel", "trail", "pause", "connect"];
+    this.dictionary = ["letter", "form", "explode", "frame","animated",
+    "entertaining", "meddle", "birds", "dead", "act", "cat", "sun", "steal", "arm", 
+    "gold", "start", "slow", "mark", "sister", "brother", "ball", "fantastic", "remind", "afraid", "trick", 
+    "cover", "stage", "ship", "step","calculator", "event", "cut", "part", "lazy", "visitor", "fork", "mouse", 
+    "land", "channel", "marvelous", "even", "solid", "shame", "hug", "year", "club", "route", 
+    "eye", "elegant", "tail", "stone", "door", "cup", "win", "notebook", "touch", "bucket", "snow", "clear",
+    "damage", "human", "salt", "pocket", "month", "arrive", "physical", "walk", "run", "shirt", "level", "battle", "walk", 
+    "scarf", "tower", "restart", "point", "raise", "communication", "taste", "men", "treat", "coach", "bead", "reproduce", "shine", 
+    "children", "great", "clap","early","popcorn", "name", "entry", "boat", "north","steal", "ancient", "seat", "alone", 
+    "fascinated", "class", "pointer", "succeed", "acoustic", "top", "fly", "foward", "pink", "look", "plastic", "lock", 
+    "friend", "false", "stop", "story", "teenager", "anger", "house", "store", "sleep", "money", "went", "detect", "obbey", 
+    "yellow", "forget", "great", "imagine", "frequent", "back", "cloud", "land","own", "snow", "pumped", "surround", 
+    "outstanding", "mail", "soap", "brave", "easy", "precious", "kick", "beard", "stuff", "kitty", 
+    "lost", "pancake", "pet", "excuse", "break", "bark", "number", "blue", "call", "ugly","skate", 
+    "remarkable", "playground", "table", "selfish", "score", "want", "invent", "push", "cool", "hospital", 
+    "judge", "land", "temporary", "line", "clock", "care", "shine", "drain","foot", "book", "dynamic",
+    "heart", "doubt", "disagree", "agree", "flag", "last", "repair", "fly", 
+    "hat", "sreen", "memory", "duck", "use", "charge", "decisive", "tree", "more","behave", 
+    "promise", "sun", "truck", "ride", "rich", "bitter", "string", "deserve", "momentous", "practice", "pop", "bells", 
+    "kind", "able", "crash", "half", "fix", "sweet", "dark", "soldier", "hammer", "name", "hard", "rabbit", "marked", 
+    "vacations", "team", "condition","cap", "handsome", "hour", "secretary", "bubble", "played", "come", "support", 
+    "stone", "monkey", "increase", "opponent", "rocket", "rat", "guide", "slow", "health", "cent", "certain", "low", 
+    "science", "earth", "space", "previous", "basketball", "pick", "damaged", "draw", "elastic", "gain", "mountain", "fail", 
+    "imagine", "manage", "guarantee", "man", "milk", "cost", "kiss", "station", "record", "fixed", "table", "boy", 
+    "ticket", "hear", "ear", "fear", "provide", "trade", "mouth", "secret", "roll", "pull", "own", "electric", "body", 
+    "cream", "legs", "fresh", "search", "cherries", "hands", "notebook", "backpack", "join", "orange", "arm", "island", 
+    "value", "donkey", "apple", "wet", "wing", "art", "friction", "main", "quick", "book", "necessary", "sleepy", 
+    "bird", "miniature", "bones", "study", "climb", "expansion", "board", "rose", "honney", "butter", "waist", "milk", 
+    "upset", "minute", "frightened", "cookie", "fire", "spark", "rare", "scream", "dinosaurs", "hard", "nails", "alone", 
+    "texture", "color", "base", "background", "trick", "five", "loose", "file", "bean", "pear", "undesirable", "notice", 
+    "trick", "crowd", "gray", "shop", "question", "bless", "prefer", "blind", "pretty", "rain","raining", "unusual", 
+    "fast", "chin", "trooper", "train", "trouble", "announce", "dream", "flowers", "show", "artist", "willing", 
+    "observe", "interrupt", "know", "middle", "select", "desire", "fruit", "dress", "jeans", "cap", "adhesive", 
+    "bent", "lips", "desert", "offend", "admire", "huge", "flash", "caption", "self", "root", "add", "grapes", 
+    "grades", "creator", "like", "shop", "reject", "treatment", "fork", "calm", "farm","swim", "garbage", 
+    "rice", "number", "branch", "eminent", "blonde", "oven", "fair", "things", "caption", "airplane", "rule", 
+    "ghost", "fear", "mountain", "permit", "high", "puppy", "computer", "hot", "bee", "thought", "natural", 
+    "three", "friendly", "sneakers", "wrong", "enormous", "papers", "apologize", "control", "test", "grass", 
+    "hypnotic", "look", "action", "thing", "cake", "pleasure", "premium", "rabbit", "punish",
+    "zoom", "brief","note", "observe", "try", "pollution", "jump", "crowded", "tour", 
+    "scare", "welcome", "rock", "order", "houses", "shoe", "birth", "round", 
+    "found", "transport", "fail", "road", "phone", "war", "brown", "plastic", 
+    "bedroom","title", "obedient", "dress", "basketball", "volleyball", "tail", "relax", 
+    "question", "lovely", "cars", "conscious","camera",  
+    "boil", "songs", "children", "purpose","hate", "brake", "arrest", "potato", 
+    "weak", "box", "party", "pot","living", "free", "ghost", "experience",
+    "smile", "trip", "describe", "suppose", "hammer", "colorful", "glove", "head", "waiting", "true", "drum", "improve", "play", 
+    "support", "drunk", "hungry", "shoes", "rely","lake", "ambiguous"];
     this.words = [];
     this.used = [];
     this.wins = 0;
@@ -11,7 +67,6 @@ class Game {
     this.wordContainer = document.querySelector('.word');
     this.input = document.querySelector('.guess');
     this.banner = document.querySelector('.banner');
-    this.maxLength = document.querySelector('.max-length');
     this.submit = document.querySelector('.submit');
     this.guesses = document.querySelector('.guesses');
     this.winsContainer = document.querySelector('.wins span');
@@ -189,7 +244,6 @@ class Game {
 
   clearInput() {
     this.input.value = "";
-    this.maxLength.innerText = '0';
     this.submit.style.marginTop = '80px';
   }
 
@@ -215,14 +269,13 @@ class Game {
     document.querySelector('.banner').style.opacity = 0;
   }
 
+  
   bindEvents() {
     this.input.addEventListener('keyup', event => {
-      if (event.keyCode == 13) {
         let input = document.querySelector('.guess').value;
         this.playerMove(input);
-      }
+      sleep(209);
       if (event.target.value.length > 0) {
-        this.maxLength.innerText = '1';
         this.submit.style.marginTop = 0;
       } else {
         this.maxLength.innerText = '0';
--- ECOLang/resources/views/hangMan.blade.php
+++ ECOLang/resources/views/hangMan.blade.php
@@ -45,7 +45,6 @@
     <div class="input">
       <label>Write a letter:
         <input autofocus type="text" class="guess" maxlength="1" onkeypress="return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.keyCode == 13) || (event.charCode == 32));">
-        <div class="max"><span class="max-length"></div>
       </label>
       <div class="submit"></div>
     </div>
